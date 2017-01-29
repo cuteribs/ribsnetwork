7d064af5eb472e7cf10b1e98e4bf5795c67dcfca
cuteribs <ericfine@yeah.net>
2017-01-30 00:25:14 +0800

    1

---
+++ privoxy/privoxy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PRIVOXY_CONF="/app/privoxy.conf"
+
+if [ ! -f "$PRIVOXY_CONF" ]; then
+	touch PRIVOXY_CONF
+	echo listen-address 0.0.0.0:$HTTP > $PRIVOXY_CONF
+	echo forward-socks5 / localhost:$SOCKS5 . >> $PRIVOXY_CONF
+fi
+
+privoxy --no-daemon $PRIVOXY_CONF
