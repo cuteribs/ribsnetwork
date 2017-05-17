6d190c714f89ae63d42ac4a21a0a4eecc5748d7d
cuteribs <ericfine@yeah.net>
2017-05-17 16:13:46 +0800

    1

--- dnspod/dnspod.sh
+++ dnspod/dnspod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NewIP=$(curl http://members.3322.org/dyndns/getip 2>/dev/null)
 Data="login_token=$Token&format=json&domain=$Domain&record_id=$RecordId&record_line_id=$RecordLineId&sub_domain=$SubDomain&value=$NewIP"
-curl -X POST https://dnsapi.cn/Record.Ddns -d ${Data} >> ~/cron.log 2>&1
+curl -X POST https://dnsapi.cn/Record.Ddns -d ${Data} >> /app/cron.log 2>&1
