c627ad899f7cda41c878874a761bca3c4d143f5c
cuteribs <ericfine1981@live.com>
2017-05-31 22:14:32 +0800

    1

--- dnspod/dnspod.sh
+++ dnspod/dnspod.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+if [ $1 ]; then
+	Token=$1
+fi
+
+if [ $2 ]; then
+	Domain=$2
+fi
+
+if [ $3 ]; then
+	SubDomain=$3
+fi
+
 NewIP=$(curl http://members.3322.org/dyndns/getip 2>/dev/null)
-Data="login_token=$Token&format=json&domain=$Domain&record_id=$RecordId&record_line_id=$RecordLineId&sub_domain=$SubDomain&value=$NewIP"
-curl -X POST https://dnsapi.cn/Record.Ddns -d ${Data} >> /app/cron.log 2>&1
+RecordId=$(curl -X POST https://dnsapi.cn/Record.List -d "login_token=$Token&format=json&domain=$Domain&sub_domain=$SubDomain" 2>/dev/null)
+RecordId=$(echo $RecordId | sed 's/.*\[{"id":"\([0-9]*\)".*/\1/')
+Result=$(curl -X POST https://dnsapi.cn/Record.Ddns -d "login_token=$Token&format=json&domain=$Domain&record_id=$RecordId&record_line_id=0&sub_domain=$SubDomain&value=$NewIP" 2>/dev/null)
+Result=$(echo $Result | sed 's/.*,"message":"\([^"]*\)".*/\1/')
+echo "$Result"
+echo "$SubDomain.$Domain => $NewIP"
--- dnspod/readme.md
+++ dnspod/readme.md
@@ -1,10 +1,8 @@
 ```
 docker run \
--e Time="* 6 * * *" \        # update at 06:00 everyday
--e Token="22222,xxxxxxxxxxxxxxxxxxxx" \          # login token for dnspod.cn
--e Domain="mydomain.com" \          # domain name
--e RecordId="222222222" \          # domain record ID
--e RecordLineId="0" \          # domain record line ID, 0 as default line ID
--e SubDomain \          # sub domain name
+-e Time="* 6 * * *" \							# update at 06:00 everyday
+-e Token="22222,xxxxxxxxxxxxxxxxxxxx" \			# login token for dnspod.cn
+-e Domain="mydomain.com" \						# domain name
+-e SubDomain="www" \							# sub domain name
 cuteribs/dnspod
 ```
