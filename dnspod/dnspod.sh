e6639f507cefb2277442ea5696e6f2eee3915873
cuteribs <ericfine1981@live.com>
2017-06-14 19:28:07 +0800

    0

--- dnspod/dnspod.sh
+++ dnspod/dnspod.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ $1 ]; then
 	LoginToken=$1
@@ -16,48 +16,57 @@ if [ -z "$SubDomain" ]; then
 	SubDomain="@"
 fi
 
-Result=""
-Code=""
-Message=""
 LastIP=""
 LastIPFile="/tmp/last_ip"
 
 if [ -f "$LastIPFile" ]; then
 	LastIP=$(cat "$LastIPFile")
+	echo "Last IP $LastIP is found."
 fi
 
 # Get new IP address
+echo "Retreiving current IP..."
 NewIP=$(wget -qO- http://members.3322.org/dyndns/getip)
+echo "Current IP $NewIP is retrieved."
 
 # Quit the script if no IP change
-if [ "$NewIP" == "$LastIP" ]; then
-	echo "$SubDomain.$Domain == $NewIP, no change made!"
+if [ "$NewIP" = "$LastIP" ]; then
+	echo "No IP change, quiting the script..."
 	exit 0
 fi
 
 # Get record ID of sub domain
+echo "Retreiving the record ID of $SubDomain.$Domain..."
 Result=$(wget -qO- --no-check-certificate --post-data "login_token=$LoginToken&format=json&domain=$Domain&sub_domain=$SubDomain" https://dnsapi.cn/Record.List)
 Code=$(echo $Result | jq -r '.status.code')
-	
-if [ "$Code" == "1" ]; then
+
+if [ "$Code" = "1" ]; then
 	RecordId=$(echo $Result | jq -r '.records[0].id')
+	echo "Record ID $RecordId exists."
+	echo "Pointing $SubDomain.$Domain to $NewIP..."
 	Result=$(wget -qO- --no-check-certificate --post-data "login_token=$LoginToken&format=json&domain=$Domain&record_id=$RecordId&record_type=A&record_line_id=0&sub_domain=$SubDomain&value=$NewIP" https://dnsapi.cn/Record.Modify)
 	Code=$(echo $Result | jq -r '.status.code')
 
-	if [ "$Code" == "1" ]; then
-		echo "$SubDomain.$Domain => $NewIP, IP updated."
+	if [ "$Code" = "1" ]; then
+		echo "$SubDomain.$Domain is now pointed to $NewIP."
 	else
+		echo "Failed to update IP of $SubDomain.$Domain."
 		echo $Result | jq -r '.status.message'
 		exit -1
 	fi	
 else
+	echo "Record ID does not exist."
+	echo "Pointing $SubDomain.$Domain to $NewIP..."
 	Result=$(wget -qO- --no-check-certificate --post-data "login_token=$LoginToken&format=json&domain=$Domain&sub_domain=$SubDomain&record_type=A&record_line_id=0&value=$NewIP" https://dnsapi.cn/Record.Create)
 	Code=$(echo $Result | jq -r '.status.code')
 
-	if [ "$Code" == "1" ]; then
+	if [ "$Code" = "1" ]; then
 		echo "$SubDomain.$Domain => $NewIP, IP updated."
 	else
+		echo "Failed to update IP of $SubDomain.$Domain."
 		echo $Result | jq -r '.status.message'
 		exit -1
 	fi	
 fi
+
+echo "$NewIP" > $LastIPFile
--- dnspod/qcloud.sh
+++ dnspod/qcloud.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $1 ]; then
-	SecretId=$1
+	SecretId=$1		# AKIDHPK4asplMiqjNmhta2GEwNYUAZibpNuB
 fi
 
 if [ $2 ]; then
-	SecretKey=$2
+	SecretKey=$2	# A4i6E6i4A0ZsugCRQ9coMgmdFclfNq55
 fi
 
 if [ $2 ]; then
@@ -20,51 +20,68 @@ if [ -z "$SubDomain" ]; then
 	SubDomain="@"
 fi
 
-Result=""
-Code=""
-Message=""
+getNounce() {
+	echo $RANDOM
+}
+
+getTimestamp() {
+	echo date '+%s'
+}
+
 LastIP=""
 LastIPFile="/tmp/last_ip"
 
 if [ -f "$LastIPFile" ]; then
 	LastIP=$(cat "$LastIPFile")
+	echo "Last IP $LastIP is found."
 fi
 
 # Get new IP address
+echo "Retreiving current IP..."
 NewIP=$(wget -qO- http://members.3322.org/dyndns/getip)
+echo "Current IP $NewIP is retrieved."
 
 # Quit the script if no IP change
-if [ "$NewIP" -eq "$LastIP" ]; then
-	echo "$SubDomain.$Domain == $NewIP, no change made!"
+if [ "$NewIP" = "$LastIP" ]; then
+	echo "No IP change, quiting the script..."
 	exit 0
 fi
 
 # Get record ID of sub domain
+echo "Retreiving the record ID of $SubDomain.$Domain..."
 Result=$(wget -qO- --no-check-certificate --post-data "login_token=$LoginToken&format=json&domain=$Domain&sub_domain=$SubDomain" https://dnsapi.cn/Record.List)
 Code=$(echo $Result | jq -r '.status.code')
-	
-if [ "$Code" -eq "1" ]; then
+
+if [ "$Code" = "1" ]; then
 	RecordId=$(echo $Result | jq -r '.records[0].id')
+	echo "Record ID $RecordId exists."
+	echo "Pointing $SubDomain.$Domain to $NewIP..."
 	Result=$(wget -qO- --no-check-certificate --post-data "login_token=$LoginToken&format=json&domain=$Domain&record_id=$RecordId&record_type=A&record_line_id=0&sub_domain=$SubDomain&value=$NewIP" https://dnsapi.cn/Record.Modify)
 	Code=$(echo $Result | jq -r '.status.code')
 
-	if [ "$Code" -eq "1" ]; then
-		echo "$SubDomain.$Domain => $NewIP, IP updated."
+	if [ "$Code" = "1" ]; then
+		echo "$SubDomain.$Domain is now pointed to $NewIP."
 	else
+		echo "Failed to update IP of $SubDomain.$Domain."
 		echo $Result | jq -r '.status.message'
 		exit -1
 	fi	
 else
+	echo "Record ID does not exist."
+	echo "Pointing $SubDomain.$Domain to $NewIP..."
 	Result=$(wget -qO- --no-check-certificate --post-data "login_token=$LoginToken&format=json&domain=$Domain&sub_domain=$SubDomain&record_type=A&record_line_id=0&value=$NewIP" https://dnsapi.cn/Record.Create)
 	Code=$(echo $Result | jq -r '.status.code')
 
-	if [ "$Code" -eq "1" ]; then
+	if [ "$Code" = "1" ]; then
 		echo "$SubDomain.$Domain => $NewIP, IP updated."
 	else
+		echo "Failed to update IP of $SubDomain.$Domain."
 		echo $Result | jq -r '.status.message'
 		exit -1
 	fi	
 fi
+
+echo "$NewIP" > $LastIPFile
 
 getSignature() {
 	echo -n "$1" | openssl dgst -sha256 -hmac "$2" -binary | openssl base64
