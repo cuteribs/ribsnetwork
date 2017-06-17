743f8c20c912892ad1fc44b80ac3564674c0faf7
cuteribs <ericfine@yeah.net>
2017-06-17 23:35:21 +0800

    1

--- node-ddns/ddns.js
+++ node-ddns/ddns.js
@@ -1,7 +1,26 @@
 #!/usr/bin/env node
+const schedule = require('node-schedule');
 const { fork } = require('child_process');
 
-var vender = process.argv[2];
+var time = process.env['ScheduleTime'] || '* 6 * * *';
+var vendor = process.argv[2];
 var params = process.argv.slice(3);
 
-var child = fork(`./${vender}.js`, params);
+console.log(`Initial DDNS request of ${vendor} is launched.`);
+console.log('------------------------------------------------------------')
+fork(`./${vendor}.js`, params)
+	.on('error', () => process.exit(1))
+	.on('close', () => {
+		console.log('------------------------------------------------------------');
+		console.log(`Initial DDNS request of ${vendor} is finished.`);
+	});
+schedule.scheduleJob('syncDDNS', time, () => {
+	console.log(`Scheduled DDNS request of ${vendor} is launched.`);
+	console.log('------------------------------------------------------------')
+	fork(`./${vendor}.js`, params)
+		.on('error', () => process.exit(1))
+		.on('close', () => {
+			console.log('------------------------------------------------------------');
+			console.log(`Scheduled DDNS request of ${vendor} is finished.`);
+		});
+});
