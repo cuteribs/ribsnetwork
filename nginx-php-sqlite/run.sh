33df03859948628c09d922f39dc2a45589028788
cuteribs <ericfine1981@live.com>
2017-11-08 22:45:34 +0800

    1

--- nginx-php-sqlite/run.sh
+++ nginx-php-sqlite/run.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
 if [ ! -d /data/www ] ; then
-  mkdir -p /data/www
-  chown nginx:www-data /data/www
-  chmod -R 777 /data/www
+	mkdir -p /data/www
+	chown nginx:www-data /data/www
+	chmod -R 777 /data/www
+fi
+
+if [ -f /data/crt/site.crt ]; then
+	if [ ! -f /etc/ssl_flag ]; then
+		cp /app/nginx.ssl.conf /etc/nginx/
+		touch /etc/ssl_flag
+	fi	
 fi
 
 # start php-fpm
-mkdir -p /data/logs
-php-fpm
+if [ ! -d /data/log ] ; then
+	mkdir -p /data/log
+fi
+
+php-fpm5
 
 # start nginx
-mkdir -p /tmp/nginx
-chown nginx /tmp/nginx
+if [ ! -d /tmp/nginx ] ; then
+	mkdir -p /tmp/nginx
+	chown nginx /tmp/nginx
+fi
+
 nginx
