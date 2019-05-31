bd6834d1f58a41d08ba6e27664ba79189724c3e0
cuteribs <ericfine1981@live.com>
2019-06-01 00:11:29 +0800

    189

--- 189spdup/189.sh
+++ 189spdup/189.sh
@@ -38,35 +38,52 @@ hashHmac() {
 
 accessToken="$1"
 method="GET"
-rate=600
+#rate=600
 extra_header="User-Agent:Apache-HttpClient/UNAVAILABLE (java 1.4)"
 
 HOST="http://api.cloud.189.cn"
 LOGIN_URL="/loginByOpen189AccessToken.action"
 ACCESS_URL="/speed/startSpeedV2.action"
 
-    echo "Sending heart_beat package <$count>"
-    split="~"
-    headers_string="$extra_header"
-    headers=`formatHeaderString "$split" "$headers_string"`
-    result=`get "$HOST$LOGIN_URL?accessToken=$accessToken" "$headers"`
-    session_key=`echo "$result" | grep -Eo "sessionKey>.*</sessionKey" | sed 's/<\/sessionKey//' | sed 's/sessionKey>//'`
-    session_secret=`echo "$result" | grep -Eo "sessionSecret>.*</sessionSecret" | sed 's/sessionSecret>//' | sed 's/<\/sessionSecret//'`
+echo "*******************************************"
+echo "发起提速请求..."
+split="~"
+headers_string="$extra_header"
+headers=`formatHeaderString "$split" "$headers_string"`
+result=`get "$HOST$LOGIN_URL?accessToken=$accessToken" "$headers"`
+session_key=`echo "$result" | grep -Eo "sessionKey>.*</sessionKey" | sed 's/<\/sessionKey//' | sed 's/sessionKey>//'`
+session_secret=`echo "$result" | grep -Eo "sessionSecret>.*</sessionSecret" | sed 's/sessionSecret>//' | sed 's/<\/sessionSecret//'`
+
+if [ -z "$session_key" ]
+then
+    echo "登录失败"
+else
     date=`env LANG=C.UTF-8 date -u '+%a, %d %b %Y %T GMT'`
     data="SessionKey=$session_key&Operate=$method&RequestURI=$ACCESS_URL&Date=$date"
     key="$session_secret"
     signature=`hashHmac "sha1" "$data" "$key"`
     headers_string="SessionKey:$session_key"${split}"Signature:$signature"${split}"Date:$date"${split}"$extra_header"
     headers=`formatHeaderString "$split" "$headers_string"`
-	qosClientSn="2cceaa8d-5721-4782-9637-faff03135779"
+    qosClientSn="2cceaa8d-5721-4782-9637-faff03135779"
     # qosClientSn=`cat /proc/sys/kernel/random/uuid`
     result=`get "$HOST$ACCESS_URL?qosClientSn=$qosClientSn" "$headers"`
-	echo $result
-	exit 0
-    echo "heart_beat:<signature:$signature>"
-    echo "date:<$date>"
-    echo -e "response:\n$result"
-    [[ "`echo ${result} | grep dialAcc`" != "" ]] &&  hint="succeeded" || hint="failed"
-    echo "Sending heart_beat package <$count> $hint"
-    echo "*******************************************"
-    sleep ${rate}
+
+    dialAccount=`echo $result | grep -Eo "<dialAccount>.*</dialAccount>" | sed 's/<dialAccount>sh:://' | sed 's/<\/dialAccount>//'`
+    baseDownRate=`echo $result | grep -Eo "<baseDownRate>.*</baseDownRate>" | sed 's/<baseDownRate>//' | sed 's/<\/baseDownRate>//'`
+    baseUpRate=`echo $result | grep -Eo "<baseUpRate>.*</baseUpRate>" | sed 's/<baseUpRate>//' | sed 's/<\/baseUpRate>//'`
+    targetDownRate=`echo $result | grep -Eo "<targetDownRate>.*</targetDownRate>" | sed 's/<targetDownRate>//' | sed 's/<\/targetDownRate>//'`
+    targetUpRate=`echo $result | grep -Eo "<targetUpRate>.*</targetUpRate>" | sed 's/<targetUpRate>//' | sed 's/<\/targetUpRate>//'`
+fi
+
+if [ -z "$dialAccount" ]
+then
+    echo "提速失败"
+else    
+    echo "提速成功"
+    echo "宽带账号: $dialAccount"
+    echo "套餐带宽: 下行 $((baseDownRate/1024)) Mbps / 上行 $((baseUpRate/1024)) Mbps"
+    echo "目前带宽: 下行 $((targetDownRate/1024)) Mbps / 上行 $((targetUpRate/1024)) Mbps"
+fi
+
+echo "*******************************************"
+exit 0
