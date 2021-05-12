39b5d4899cd8cb1909aa73c3d61d8d2c2e929ab0
Cuteribs <ericfine1981@live.com>
2021-05-12 22:26:13 +0800

    2

--- bing-daily/bing-daily.ps1
+++ bing-daily/bing-daily.ps1
@@ -1,42 +1,39 @@
 ﻿$wallpaperPath = "$HOME\wallpaper.jpg"
 $today = Get-Date -Format "yyyy-MM-dd"
-echo $today
-$wallpaperUri = "https://r5ea.blob.core.windows.net/bingdaily/wallpapers/2021/05/2021-05-11.jpg"
 
+if (Test-Path $wallpaperPath) {
+  $fileDate = (Get-Item $wallpaperPath).LastWriteTime.ToString("yyyy-MM-dd")
+  
+  if($today -eq $fileDate) {
+    Write-Host "No need to update"
+    exit
+  }
+}
 
-$setWallpaperSrc = @"
+Write-Host "Downloading Bing Daily Wallpaper"
+
+$baseUrl = "https://cn.bing.com"
+$xmlUrl = "$baseUrl/HPImageArchive.aspx?format=xml&idx=0&n=1&uhd=1&uhdwidth=3840&uhdheight=2592"
+$imgUrl = ([xml](Invoke-WebRequest $xmlUrl).Content).SelectNodes("/images/image[1]/urlBase") | Select-Object -Expand "#text"
+$wallpaperUri = "$baseUrl$imgUrl" + "_UHD.jpg&w=3840&h=2592&rs=1&c=1&pid=hp"
+Invoke-WebRequest $wallpaperUri -OutFile $wallpaperPath
+
+$src = @"
 using System.Runtime.InteropServices;
 
 public class Wallpaper
 {
-  public const int SetDesktopWallpaper = 20;
-  public const int UpdateIniFile = 0x01;
-  public const int SendWinIniChange = 0x02;
-
   [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
   private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
 
   public static void SetWallpaper(string path)
   {
-    SystemParametersInfo(SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange);
+    SystemParametersInfo(20, 0, path, 0x01 | 0x02);
   }
 }
 "@
-Add-Type -TypeDefinition $setWallpaperSrc
-
-
-# Invoke-WebRequest $wallpaperUri -OutFile $wallpaperPath
-
-# [Wallpaper]::SetWallpaper($wallpaperPath)
-
-[Windows.System.UserProfile.LockScreen,Windows.System.UserProfile,ContentType=WindowsRuntime]
-
-$image = Await ([Windows.Storage.StorageFile]::GetFileFromPathAsync($wallpaperPath)) ([Windows.Storage.StorageFile])
-AwaitAction ([Windows.System.UserProfile.LockScreen]::SetImageFileAsync($image))
-
-
-
-
+Add-Type -TypeDefinition $src
+[Wallpaper]::SetWallpaper($wallpaperPath)
 
 
 
