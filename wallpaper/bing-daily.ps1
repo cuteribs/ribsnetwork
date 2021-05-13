ddd5ccd4ff70c9c54eb317c0f7ae26fceff8a1a5
Eric Liu <li.eric.liu@dnv.com>
2021-05-13 18:00:54 +0800

    wallbingper

---
+++ wallpaper/bing-daily.ps1
@@ -0,0 +1,41 @@
+﻿$wallpaperPath = "$HOME\wallpaper.jpg"
+$today = (Get-Date).ToUniversalTime().ToString("yyyy-MM-dd")
+
+if (Test-Path $wallpaperPath) {
+  $fileDate = (Get-Item $wallpaperPath).LastWriteTime.ToString("yyyy-MM-dd")
+  
+  if ($today -eq $fileDate) {
+    Write-Host "No need to update"
+  }
+}
+else {
+  Write-Host "Downloading Bing Daily Wallpaper"
+  
+  $baseUrl = "https://cn.bing.com"
+  $xmlUrl = "$baseUrl/HPImageArchive.aspx?format=xml&idx=0&n=1&uhd=1&uhdwidth=3840&uhdheight=2592"
+  $imgUrl = ([xml](Invoke-WebRequest $xmlUrl).Content).SelectNodes("/images/image[1]/urlBase") | Select-Object -Expand "#text"
+  $wallpaperUri = "$baseUrl$imgUrl" + "_UHD.jpg&w=3840&h=2592&rs=1&c=1&pid=hp"
+  Write-Host $wallpaperUri
+  Invoke-WebRequest $wallpaperUri -OutFile $wallpaperPath
+}
+
+$src = @"
+using System.Runtime.InteropServices;
+
+public class Wallpaper
+{
+  public const int SetDesktopWallpaper = 20;
+  public const int UpdateIniFile = 0x01;
+  public const int SendWinIniChange = 0x02;
+
+  [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+  private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+  public static void SetWallpaper(string path)
+  {
+    SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+  }
+}
+"@
+Add-Type -TypeDefinition $src
+[Wallpaper]::SetWallpaper($wallpaperPath)
---
+++ wallpaper/schedule.ps1
@@ -0,0 +1,21 @@
+﻿$myPath = $MyInvocation.MyCommand.Path;
+$jobName = "BingDailyWallpaper"
+$job = Get-ScheduledJob | Where-Object Name -eq $jobName
+
+if (($Args.Length -gt 0) -And ($Args[0] -eq "uninstall")) {
+    if ($job) {
+        Unregister-ScheduledJob -Name $jobName
+    }
+    
+    Write-Host("$jobName job removed")
+    Exit
+}
+
+if (-Not ($job)) {
+    $job = Register-ScheduledJob -Name $jobName -FilePath $myPath
+    $trigger1 = New-JobTrigger -Daily -At 00:00
+    $trigger2 = New-JobTrigger -Daily -At 12:00
+    Add-JobTrigger -Trigger $trigger1 -Name $jobName
+    Add-JobTrigger -Trigger $trigger2 -Name $jobName
+    Write-Host("$jobName job added")
+}
